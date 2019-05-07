426bb9f8f9a4d1e1c9bd0f2b8a535f8c36f044df
Martin Guillon <martin.guillon@akylas.fr>
2019-05-07 16:11:14 +0200

    fix: update to latest material components to fix css overloading

--- src/Fab.vue
+++ src/Fab.vue
@@ -4,13 +4,13 @@
         <StackLayout class="fab-items-holder" :horizontalAlignment="position" row="2" colSpan="3" isPassThroughParentEnabled="true">
             <slot />
         </StackLayout>
-        <GridLayout :columns="isRight ? '*,auto,auto' : 'auto,auto,*'" padding="8" width="100%" :horizontalAlignment="position" row="3" colSpan="3" isPassThroughParentEnabled="true" >
+        <GridLayout :columns="isRight ? '*,auto,auto' : 'auto,auto,*'"  width="100%" :horizontalAlignment="position" row="3" colSpan="3" isPassThroughParentEnabled="true" >
             <MDCardView col="1" ref="titleLabel" class='fab-title-holder' :class="computedTitleClass" :visibility="title?'visible':'collapsed'" :isUserInteractionEnabled="active" @tap="onButtonTap">
                 <StackLayout orientation="horizontal" height="100%">
                     <Label class="fab-item-title" :text="title" />
                 </StackLayout>
             </MDCardView>
-            <GridLayout :col="isRight? 2 : 0">
+            <GridLayout :col="isRight? 2 : 0" >
                 <MDButton class="fab-button" @tap="onButtonTap" :color="color" :backgroundColor="backgroundColor"/>
                 <label class="fab-icon fab-icon-off" :class="iconOffClass" :color="color" :text="icon" isUserInteractionEnabled="false" style="z-index:20;"/>
                 <Label  class="fab-icon fab-icon-on" :class="iconOnClass" :color="color" :text="iconOn || icon" isUserInteractionEnabled="false" style="z-index:20;"/>
@@ -19,6 +19,4 @@
 
     </GridLayout>
 </template>
-
-
 <script src="./Fab.ts"/>
