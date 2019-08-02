5b2fc73de82e5964ac0589e60ad176dc32a7f901
Martin Guillon <martin.guillon@akylas.fr>
2019-08-02 21:34:15 +0200

    fix: some fixes

--- src/Fab.vue
+++ src/Fab.vue
@@ -1,14 +1,12 @@
 <template>
     <GridLayout left="0" top="0" height="100%" width="100%" rows="auto,*,auto,auto" columns="auto,*,auto" isPassThroughParentEnabled="true">
         <AbsoluteLayout colSpan="3" rowSpan="4" class="fab-backdrop" :class="classBackdrop" @tap="onBackdropTap" :isUserInteractionEnabled="active" :debug="debug" :visibility="realActive?'visible':'collapsed'" />
-        <StackLayout class="fab-items-holder" :horizontalAlignment="position" row="2" colSpan="3" isPassThroughParentEnabled="true" :visibility="realActive?'visible':'collapsed'"  :debug="debug">
+        <StackLayout class="fab-items-holder" :horizontalAlignment="position" row="2" colSpan="3" isPassThroughParentEnabled="true" :visibility="realActive?'visible':'collapsed'" :debug="debug">
             <slot />
         </StackLayout>
 
         <GridLayout :columns="isRight ? '*,auto,auto' : 'auto,auto,*'" width="100%" :horizontalAlignment="position" row="3" colSpan="3" isPassThroughParentEnabled="true">
-            <StackLayout orientation="horizontal" height="100%" col="1" ref="titleLabel" class='fab-title-holder' :class="computedTitleClass" :visibility="title?'visible':'collapsed'" :isUserInteractionEnabled="active" @tap="onButtonTap" :debug="debug">
-                <Label class="fab-item-title" :text="title" />
-            </StackLayout>
+            <Label col="1" class="fab-title" :text="title" :class="computedTitleClass" v-if="title" :isUserInteractionEnabled="active" @tap="onButtonTap" :debug="debug" />
             <GridLayout class="fab-button" :col="isRight? 2 : 0" @tap="onButtonTap" :color="color" :rippleColor="actualRippleColor" :backgroundColor="actualBackgroundColor" :debug="debug">
                 <!-- <MDButton class="fab-button" @tap="onButtonTap" :color="color" :rippleColor="rippleColor" :backgroundColor="backgroundColor" :debug="debug" /> -->
                 <label class="fab-icon fab-icon-off" :class="iconOffClass" :color="color" :text="icon" isUserInteractionEnabled="false" style="z-index:20;" />
--- src/FabItem.vue
+++ src/FabItem.vue
@@ -1,8 +1,7 @@
 <template>
     <GridLayout class="fab-item-holder" :horizontalAlignment="position" :columns="isRight ? '*,auto,auto' : 'auto,auto,*'" width="100%" isPassThroughParentEnabled="true" :debug="debug">
-        <Label orientation="horizontal" height="100%" col="1" ref="titleLabel" class="fab-item-title-holder fab-item-title" :class="computedTitleClass" :visibility="title?'visible':'collapsed'" :isUserInteractionEnabled="visible" @tap="onButtonTap" :debug="debug" />
-            <!-- <Label class="fab-item-title" :text="title" /> -->
         <MDButton :col="isRight ? 2 : 0" ref="button" class="fab-item" :class="computedButttonClass" :color="color" :rippleColor="rippleColor" :backgroundColor="backgroundColor" :text="icon" @tap="onButtonTap" :isUserInteractionEnabled="visible" :debug="debug" />
+        <Label  col="1" class="fab-item-title" :class="computedTitleClass" :text="title" v-if="title" :isUserInteractionEnabled="visible" @tap="onButtonTap" :debug="debug" />
     </GridLayout>
 </template>
 
