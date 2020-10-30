a2542939d21385952b73077e1d74cc8d901c8ecd
Martin Guillon <martin.guillon@akylas.fr>
2020-10-30 17:31:52 +0100

    fix: support colors as props

--- src/FabBase.ts
+++ src/FabBase.ts
@@ -1,12 +1,13 @@
+import { Color } from '@nativescript/core';
 import Vue from 'nativescript-vue';
 import { Prop } from 'vue-property-decorator';
 
 export default abstract class FabBase extends Vue {
     @Prop({ type: String }) icon: string;
     @Prop({ type: String }) title?: string;
-    @Prop({ type: String }) backgroundColor?: string;
-    @Prop({ type: String }) color?: string;
-    @Prop({ type: String }) rippleColor?: string;
+    @Prop() backgroundColor?: string | Color;
+    @Prop() color?: string | Color;
+    @Prop() rippleColor?: string | Color;
 
     @Prop({ type: String }) buttonClass?: string;
     @Prop({ type: String }) titleClass?: string;
